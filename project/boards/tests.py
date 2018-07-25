365c9b48a09271b7fd9a8bec749d2dbc74238b22
Ana Richardson <anarichardson@Anas-MacBook-Pro.local>
2018-07-25 10:42:27 +0200

    Bootstrap added

--- project/boards/tests.py
+++ project/boards/tests.py
@@ -1,3 +1,20 @@
+from django.urls import reverse, resolve
+# from django.core.urlresolvers import reverse # module was moved to django.urls
 from django.test import TestCase
 
-# Create your tests here.
+from boards.views import home
+
+
+class HomeTests(TestCase):
+    """Test the status code of the response."""
+
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    """Test correct view function for requested URL."""
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
--- project/boards/views.py
+++ project/boards/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+from boards.models import Board
+
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    # import Board model and list all existing boards
+    boards = Board.objects.all()
+    # boards_names = list()
+    #
+    # for board in boards:
+    #     boards_names.append(board.name)
+    #
+    # names = '<br>'.join(boards_names)
+    #
+    # return HttpResponse(names)
+    return render(request, 'home.html', {'boards': boards})
