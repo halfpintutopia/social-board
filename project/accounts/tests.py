2c720f659a67497d6e1d9a4471ce74595f75f2fa
Ana Richardson <anarichardson@anas-mbp-1.home>
2018-07-29 07:56:24 +0200

    Added custom signup form and tests

--- project/accounts/tests.py
+++ project/accounts/tests.py
@@ -1,15 +1,86 @@
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve, reverse
 
 from accounts.views import signup
+from .forms import SignUpForm
 
 
 class SignUpTests(TestCase):
-    def test_signup_status_code(self):
+    def setUp(self):
         url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
         self.assertEquals(view.func, signup)
+
+    def csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        """
+        Case: if a new developer wanted to re-use the SignUpForm for something else,
+        then those new fields would also show up in the signup.html or the change could pass unnoticed.
+        Test: verifies the HTML inputs in the template.
+        The view must contain five inputs: csrf, username, email, password1, password2
+        """
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'testuser123',
+            'email': 'test@test.com',
+            'password1': 'qwertz123',
+            'password2': 'qwertz123'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirection(self):
+        """A valid form submission should redirect the user to the home page."""
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authenticate(self):
+        """
+        Create a new request to an arbitary page.
+        The resulting response should now have 'user' to its context,
+        after a successful sign up.
+        """
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+
+class InvalidSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {})  # submit an empty dictionary.
+
+    def test_signup_status_code(self):
+        """An invalid form submission should return to the same page."""
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,7 +1,18 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+# instead of using UserCreationForm to add the Email Field
+from .forms import SignUpForm
 
 
 def signup(request):
-    form = UserCreationForm()
+    """Logic of signup view."""
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
